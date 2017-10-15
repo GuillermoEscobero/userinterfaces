180ce9280feca7f07f7b01b5045f157798fd3804
Raul Olmedo <100346073@alumnos.uc3m.es>
2017-10-15 19:07:08 +0200

    Changed format according to linter

--- Ex5/js/script.js
+++ Ex5/js/script.js
@@ -1,75 +1,72 @@
-function onLoginSubmit() {
-    //Obtain the user and password entered in the modal popup
-    var userEmail = document.getElementById("login-email").value;
-    var userPassword = document.getElementById("login-password").value;
+function onLoginSubmit () {
+  // Obtain the user and password entered in the modal popup
+  var userEmail = document.getElementById('login-email').value
+  var userPassword = document.getElementById('login-password').value
 
-    //Hide modal
-    document.getElementById("login-modal").style.display = "none";
+  // Hide modal
+  document.getElementById('login-modal').style.display = 'none'
 
-    //Check if it's the first time the user logs in
-    if (getCookie("email") != userEmail) {
-        //Create a new cookie with the information of the user
-        setCookie("email", userEmail, 30);
-        setCookie("password", userPassword, 30);
-    }
-    else {
-        loadDataOnCookie();
-    }
-     
+  // Check if it's the first time the user logs in
+  if (getCookie('email') !== userEmail) {
+    // Create a new cookie with the information of the user
+    setCookie('email', userEmail, 30)
+    setCookie('password', userPassword, 30)
+  } else {
+    loadDataOnCookie()
+  }
 }
 
-function setCookie(cookieName, cookieValue, expDays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (expDays * 24 * 60 * 60 * 1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+function setCookie (cookieName, cookieValue, expDays) {
+  var d = new Date()
+  d.setTime(d.getTime() + (expDays * 24 * 60 * 60 * 1000))
+  var expires = 'expires=' + d.toGMTString()
+  document.cookie = cookieName + '=' + cookieValue + ';' + expires + ';path=/'
 }
 
-function getCookie(cookieName) {
-    var name = cookieName + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
+function getCookie (cookieName) {
+  var name = cookieName + '='
+  var decodedCookie = decodeURIComponent(document.cookie)
+  var ca = decodedCookie.split(';')
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i]
+    while (c.charAt(0) === ' ') {
+      c = c.substring(1)
     }
-    return "";
-
+    if (c.indexOf(name) === 0) {
+      return c.substring(name.length, c.length)
+    }
+  }
+  return ''
 }
 
-function loadDataOnCookie() {
-    //TODO: load the name and the profile picture
+function loadDataOnCookie () {
+  // TODO: load the name and the profile picture
 }
 
 function onIconClick (buttonId) {
-    //TODO: check if it works in other web browsers -apart from firefox-
-    var textNode = document.getElementById(buttonId).lastChild;
+  // TODO: check if it works in other web browsers -apart from firefox-
+  var textNode = document.getElementById(buttonId).lastChild
 
-    if (textNode.previousSibling.className == "fa fa-heart-o") {
-        textNode.textContent = " " + addOne(textNode.textContent) + " Likes ";
-    }
-    else
-        textNode.textContent = " " + addOne(textNode.textContent) + " Times Shared ";
+  if (textNode.previousSibling.className === 'fa fa-heart-o') {
+    textNode.textContent = ' ' + addOne(textNode.textContent) + ' Likes '
+  } else {
+    textNode.textContent = ' ' + addOne(textNode.textContent) + ' Times Shared '
+  }
 }
 
 function addOne (textToIncrement) {
-    var number = parseInt(textToIncrement);
-    return number + 1;
+  var number = parseInt(textToIncrement)
+  return number + 1
 }
 
 function onDragCard () {
-    console.log("Item dragged");
+  console.log('Item dragged')
 }
 
-function onDropCard() {
-    console.log("Item dropped");
+function onDropCard () {
+  console.log('Item dropped')
 }
 
-//TODO: drag & drop part
-//TODO: cookies part
-//TODO: show description when button "+" clicked
+// TODO: drag & drop part
+// TODO: cookies part
+// TODO: show description when button '+' clicked
